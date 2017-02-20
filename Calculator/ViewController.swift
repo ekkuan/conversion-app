61b0c52e27c13fd66bc75b96b6244622a50e4afb
ekkuan <erickkuan@gmail.com>
2017-02-20 01:36:49 -0800

    Touch interface and conversion implemented.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblResults: UITextField!
+    
+    var result = Float()
+    var currentNumber = Float()
+    
+    var currentOp = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +28,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnNumberInput(_ sender: UIButton) {
+        currentNumber = currentNumber * 10 + Float(Int(sender.titleLabel!.text!)!)
+        lblResults.text = ("\(Int(currentNumber))\(" cup")")
+    }
+
+
+
+    @IBAction func btnConvert(_ sender: UIButton) {
+        result = currentNumber * 237
+        lblResults.text = ("\(Int(result))\(" gram")")
+        currentNumber = 0
+        
+    }
+    
+
+    @IBAction func btnClear(_ sender: UIButton) {
+        result = 0
+        currentNumber = 0
+        currentOp = "="
+        lblResults.text = ("\(result)")
+    }
 
 }
 
